c75bfd1ebc457b191ff5028a974821459285baf1
Mimi Flores <aflores393@cps.edu>
2025-04-24 09:57:27 -0500

    neural nets

--- a8.py
+++ a8.py
@@ -4,7 +4,7 @@ print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
 xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 
-xorn = NeuralNet(2, 2
-, 1)
+# xorn = NeuralNet(2, 2, 1)
+xorn = NeuralNet(2, 8, 1)
 xorn.train(xor_training_data)
 print(xorn.test_with_expected(xor_training_data))
